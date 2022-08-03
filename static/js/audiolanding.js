820f22fc42068549460cc83ac034d564fbadd84f
TomatoFT <thomas.lezard.2002@gmail,com>
2022-08-03 20:44:14 +0700

    Deploy

---
+++ static/js/audiolanding.js
@@ -0,0 +1,7 @@
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 800);
+});
